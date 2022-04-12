b89ab65987dccd84bf9ffbd31872f7eaa3727acd
Shubham Chaudhari <88569766+Shubham7276@users.noreply.github.com>
2022-04-12 15:25:41 +0530

    Delete Componants directory

--- Componants/src/componant/Firstchild.jsx
+++
@@ -1,7 +0,0 @@
-
-function Firstchild(){
-    return(
-        <h4>First child componant</h4>
-    )
-}
-export default Firstchild;
--- Componants/src/componant/Secondcomponant.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Secondcomponant(){
-    return(
-        <h3>Second Componant</h3>
-    );
-}
-
-export default Secondcomponant;
