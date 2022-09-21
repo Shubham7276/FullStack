ef09138ddf41158ee95803915586ef7fcd772f47
Shubham Chaudhari <88569766+Shubham7276@users.noreply.github.com>
2022-09-21 16:25:40 +0530

    Update index.js

--- 1.Ticket-App-In-React/server/index.js
+++ 1.Ticket-App-In-React/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const morgan = require("morgan");
+
 const cookieParser = require("cookie-parser");
 const expressValidator = require('express-validator');
 const cors = require('cors');
